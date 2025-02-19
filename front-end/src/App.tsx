21f6a7a927173e24f7c8493efa44ca427811edf5
Jesslyn Caroline <158556535+Earlade@users.noreply.github.com>
2025-02-19 22:22:15 +0700

    change browserrouter into hashrouter

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Layout from "./pages/Layout.tsx";
 import Home from "./pages/Home.tsx";
@@ -8,7 +8,7 @@ import Archive from "./pages/Archive.tsx";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />}/>
@@ -17,7 +17,7 @@ function App() {
           <Route path="archive" element={<Archive />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   )
 }
 
