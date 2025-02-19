673b1ab8bbee77c145ffe175bd6885590aa68ac1
Jesslyn Caroline <158556535+Earlade@users.noreply.github.com>
2025-02-19 23:08:22 +0700

    update

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,11 +1,8 @@
 function Home() {
     return (
-    <div className={`w-full h-full flex flex-col`}>
+    <div className={`w-full h-full flex flex-col gap-5`}>
         <div className={`w-full`}>
             <input type="text" id="search" className={`w-full h-10 outline-none bg-white rounded px-3 text-sm`} placeholder="Search note"/>
-        </div>
-        <div className={``}>
-
         </div>
     </div>
 )}
--- front-end/src/pages/NewNote.tsx
+++ front-end/src/pages/NewNote.tsx
@@ -1,5 +1,11 @@
 function NewNote() {
-    return <h1>NewNote</h1>
+    return (
+    <div className={`w-full h-full flex flex-col gap-5`}>
+        <div className={`w-full`}>
+            <input type="text" id="title" className={`w-full h-10 outline-none bg-white rounded px-3 text-sm font-bold`} placeholder="Note Title"/>
+        </div>
+    </div>
+    )
 }
 
 export default NewNote;
