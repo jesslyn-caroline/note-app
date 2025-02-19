6201dbfad1b44bcb23193389eac446a24078e11f
Jesslyn Caroline <158556535+Earlade@users.noreply.github.com>
2025-02-19 22:24:58 +0700

    update

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "./pages/Layout.tsx";
 import Home from "./pages/Home.tsx";
@@ -8,7 +8,7 @@ import Archive from "./pages/Archive.tsx";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />}/>
@@ -17,7 +17,7 @@ function App() {
           <Route path="archive" element={<Archive />} />
         </Route>
       </Routes>
-    </Router>
+    </BrowserRouter>
   )
 }
 
