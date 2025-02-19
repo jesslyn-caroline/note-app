e3c827080ca471e92a812f62a80a3a1ce749f8a4
Jesslyn Caroline <158556535+Earlade@users.noreply.github.com>
2025-02-19 22:29:28 +0700

    update

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Layout from "./pages/Layout.tsx";
 import Home from "./pages/Home.tsx";
@@ -8,17 +8,17 @@ import Archive from "./pages/Archive.tsx";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />}/>
-          <Route path="public" element={<Public />}/>
+          <Route index element={<Home />} />
+          <Route path="public" element={<Public />} />
           <Route path="new-note" element={<NewNote />} />
           <Route path="archive" element={<Archive />} />
         </Route>
       </Routes>
-    </BrowserRouter>
-  )
+    </Router>
+  );
 }
 
 export default App;
